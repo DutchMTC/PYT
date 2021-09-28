5d6c264e2c11a3adc69af96000c974b5ba596072
Miyaki Truong <32574@ma-web.nl>
2021-09-28 09:39:11 +0200

    First Commit

---
+++ String.py
@@ -0,0 +1,3 @@
+a = "Firetruck"
+
+print(a[0:1] + a[6:9])
---
+++ berekeningen.py
@@ -0,0 +1,41 @@
+import time
+
+a = 2
+b = 5
+c = 4
+d = 234
+e = 12498
+f = 10000
+g = 125
+h = 1
+i = 8
+
+print("2 + 2 =")
+time.sleep(0.5)
+print(a + a)
+print("")
+time.sleep(1)
+
+print("5 - 4 =")
+time.sleep(0.5)
+print(b - c)
+print("")
+time.sleep(1)
+
+print("234 x 12498 =")
+time.sleep(0.5)
+print(d * e)
+print("")
+time.sleep(1)
+
+print("10000 / 125 =")
+time.sleep(0.5)
+print(f / g)
+print("")
+time.sleep(1)
+
+print("1 / 8 =")
+time.sleep(0.5)
+print(h % i)
+print("")
+time.sleep(1)
